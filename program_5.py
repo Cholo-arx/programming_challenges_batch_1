9ec077db32b0371ff30e866b6bb98dfe647cad10
cholo-arx <artcholocequena2007@gmail.com>
2026-03-09 22:53:59 +0800

    all program has been finished to my known methods

---
+++ program_5.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+number_1 = float(input("input the first number: "))
+number_2 = float(input("input the second number: "))
+# quotient operation and display
+print(f"the sum of {number_1} and {number_2} is {number_1 / number_2}")
